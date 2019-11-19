bb78a374e6436e21ca03406d3a3cdfd19580dc0a
Ayat Rashad <ayat.rashad@gmail.com>
2019-11-19 16:01:52 +0100

    voc dataset

--- code/util.py
+++ code/util.py
@@ -10,30 +10,48 @@ import torch.nn.functional as F
 
 def get_voc_data(dataset='seg'):
     batch_size = 64
+    shuffle = False
     
     transformations = transforms.Compose([
     #transforms.Resize(255),
-    #transforms.CenterCrop(224),
+    transforms.CenterCrop(224),
     transforms.ToTensor(),
-    transforms.Normalize((0.1307,), (0.3081,))
-        #mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
+    transforms.Normalize(
+        #(0.1307,), (0.3081,))
+        mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
+    )
     ])
     
-    if dataset == 'seg':
-        dataset = datasets.VOCSegmentation('../data', image_set="train", download=True, transform=transformations)
-    elif dataset == 'det':
-        dataset = datasets.VOCDetection('../data', image_set="train", download=True, transform=transformations)
-
+    trgt_transformations = transforms.Compose([
+    #transforms.Resize(255),
+    transforms.CenterCrop(224),
+    transforms.ToTensor(),
+    ])
+    
+    dataset_train = datasets.VOCSegmentation('../data', year='2008', image_set="train", download=True,
+                       transform=transformations, target_transform=trgt_transformations)
+    dataset_test = datasets.VOCSegmentation('../data', year='2008', image_set="val", download=True,
+                       transform=transformations, target_transform=trgt_transformations)
+    
     train_loader = torch.utils.data.DataLoader(
-            dataset,
-            batch_size=batch_size, shuffle=True)
+        dataset_train,
+        batch_size=batch_size,
+        shuffle=shuffle,
+        num_workers=4,
+        **kwargs)
     
     test_loader = torch.utils.data.DataLoader(
-            dataset,
-            batch_size=batch_size, shuffle=True)
+        dataset_test,
+        batch_size=batch_size,
+        num_workers=4,
+        shuffle=shuffle, **kwargs)   
     
     return train_loader, test_loader
 
 
 def get_coco_data():
     pass
+
+
+# Evaluation metrics
+
