8dd478cbf68b57a3ab0ce743c610ac73182cdbf8
Ayat Rashad <ayat.rashad@gmail.com>
2020-01-25 21:29:57 +0100

    testing layers script

---
+++ code/test_layers.sh
@@ -0,0 +1,12 @@
+# setup (python2) environment
+pip install -r requirements.txt
+
+# setup deformable convolution operator
+cd deform_conv
+bash prepare.sh
+cd ..
+
+# test 1,2,3 deformable layers
+python main.py --layers=1
+python main.py --layers=2
+python main.py --layers=3
